cc37723561d2751fe4174665dfcc2be79757a61b
MasterSon-git <57006526+MasterSon-git@users.noreply.github.com>
2024-03-08 20:27:10 +0100

    make the button work
    target pop for usa reduced by half but min 3 million pops

--- scripts/copy-to-vic3-kmpmp.ps1
+++ scripts/copy-to-vic3-kmpmp.ps1
@@ -1,3 +1,4 @@
 robocopy "..\src" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp" /MIR /XF thumbnail* /XD .metadata
-copy "..\src\.metadata\metadata_kmpmp.json" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp\.metadata\metadata.json"
-copy "..\src\thumbnail_kmpmp.png" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp\thumbnail.png"
+New-Item "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp\.metadata" -Type directory -Force
+Copy-Item "..\src\.metadata\metadata_kmpmp.json" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp\.metadata\metadata.json" -Force
+Copy-Item "..\src\thumbnail_kmpmp.png" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kmpmp\thumbnail.png" -Force
--- scripts/copy-to-vic3-kspmp.ps1
+++ scripts/copy-to-vic3-kspmp.ps1
@@ -1,5 +1,7 @@
 robocopy "..\src" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp" /MIR /XF thumbnail* /XD .metadata defines map
-copy "..\src\.metadata\metadata_kspmp.json" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\.metadata\metadata.json"
-copy "..\src\thumbnail_kspmp.png" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\thumbnail.png"
-copy "..\src\common\defines\kmpmp_00_defines.txt" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\kmpmp_00_defines.txt"
-copy "..\src\common\defines\vtm_defines.txt" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\vtm_defines.txt"
+New-Item "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\.metadata" -Type directory -Force
+Copy-Item "..\src\.metadata\metadata_kspmp.json" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\.metadata\metadata.json" -Force
+Copy-Item "..\src\thumbnail_kspmp.png" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\thumbnail.png" -Force
+New-Item "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines" -Type directory -Force
+Copy-Item "..\src\common\defines\kmpmp_00_defines.txt" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\kmpmp_00_defines.txt" -Force
+Copy-Item "..\src\common\defines\vtm_defines.txt" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\vtm_defines.txt" -Force
