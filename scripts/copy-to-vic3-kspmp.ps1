933dc30a6ce31b31d6630dec3360c5c63978858d
MasterSon-git <57006526+MasterSon-git@users.noreply.github.com>
2024-04-19 17:06:20 +0200

    copy map textures for kspmp

--- scripts/copy-to-vic3-kspmp.ps1
+++ scripts/copy-to-vic3-kspmp.ps1
@@ -1,4 +1,5 @@
 robocopy "..\src" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp" /MIR /XF thumbnail* /XD .metadata defines map
+robocopy "..\src\gfx\map\textures" "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\gfx\map\textures" /MIR 
 New-Item "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\.metadata" -Type directory -Force
 Copy-Item "..\src\.metadata\metadata_kspmp.json" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\.metadata\metadata.json" -Force
 Copy-Item "..\src\thumbnail_kspmp.png" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\thumbnail.png" -Force
