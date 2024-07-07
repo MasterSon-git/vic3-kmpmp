03d2e0a13f5e0483a0014e9b57872de86202d5ab
MasterSon-git <57006526+MasterSon-git@users.noreply.github.com>
2024-07-07 18:06:14 +0200

    fix copy script

--- scripts/copy-to-vic3-kspmp.ps1
+++ scripts/copy-to-vic3-kspmp.ps1
@@ -5,4 +5,3 @@ Copy-Item "..\src\.metadata\metadata_kspmp.json" -Destination "$env:UserProfile\
 Copy-Item "..\src\thumbnail_kspmp.png" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\thumbnail.png" -Force
 New-Item "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines" -Type directory -Force
 Copy-Item "..\src\common\defines\kmpmp_00_defines.txt" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\kmpmp_00_defines.txt" -Force
-Copy-Item "..\src\common\defines\vtm_defines.txt" -Destination "$env:UserProfile\Documents\Paradox Interactive\Victoria 3\mod\vic3-kspmp\common\defines\vtm_defines.txt" -Force
